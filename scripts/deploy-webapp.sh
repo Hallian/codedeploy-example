61526ebaca7da9d876bb4b870870c8437dd7a0f6
Nikolas Lahtinen <nikolas.lahtinen@gmail.com>
2017-09-02 18:53:56 +0300

    working deployment
    feat: add package.json for webapp
    fix: node_modules added to package
    feat: deployment script only describes stack once
    feat: associate public ip for ssh access

--- scripts/deploy-webapp.sh
+++ scripts/deploy-webapp.sh
@@ -4,14 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 REVISION=$1
 
-DEPLOYMENT_GROUP=`aws cloudformation describe-stacks --stack-name webapp-example --query "Stacks[0].Outputs | [?OutputKey == 'DeploymentGroup'].OutputValue" --output text`
-DEPLOYMENT_BUCKET=`aws cloudformation describe-stacks --stack-name webapp-example --query "Stacks[0].Outputs | [?OutputKey == 'DeploymentBucket'].OutputValue" --output text`
-APPLICATION_NAME=`aws cloudformation describe-stacks --stack-name webapp-example --query "Stacks[0].Outputs | [?OutputKey == 'ApplicationName'].OutputValue" --output text`
+STACK_OUTPUTS=`aws cloudformation describe-stacks --stack-name webapp-example --query "Stacks[0].Outputs[].[OutputValue]" --output text`
+DEPLOYMENT_BUCKET=`echo "$STACK_OUTPUTS" | grep webappdeploymentbucket`
+DEPLOYMENT_GROUP=`echo "$STACK_OUTPUTS" | grep WebappDeploymentGroup`
+APPLICATION_NAME=`echo "$STACK_OUTPUTS" | grep WebappApplication`
 
 aws deploy push --application-name $APPLICATION_NAME \
 	--s3-location s3://$DEPLOYMENT_BUCKET/$REVISION \
-	--source $DIR/../webapp
+	--source $DIR/../webapp > /dev/null
 
 aws deploy create-deployment --application-name $APPLICATION_NAME \
-	--s3-location bucket=$DEPLOYMENT_BUCKET,key=$REVISION,bundleType=zip \
+	--s3-location bucket="$DEPLOYMENT_BUCKET",key="$REVISION",bundleType=zip \
 	--deployment-group-name $DEPLOYMENT_GROUP
